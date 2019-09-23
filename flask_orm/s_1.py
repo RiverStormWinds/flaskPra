e94567d0b05a3c68038ccfeb6ac25f557705c974
RiverStormWinds <1328897526@qq.com>
2019-09-23 18:13:10 +0800

    ThinkPad_20190923

--- flask_orm/s_1.py
+++ flask_orm/s_1.py
@@ -1,4 +1,28 @@
-import sqlalchemy
+# coding:utf-8
+from sqlalchemy import create_engine
+
+engine = create_engine('mysql+pymysql://root:root@127.0.0.1:3306/test',
+                       max_overflow=0, pool_size=5)
+
+def task(arg):
+    conn = engine.contextual_connect()
+    with conn:
+        cur = conn.execute(
+            'select * from test'
+        )
+        result = cur.fetchall()
+        print(result)
+
+# for i in range(20):
+#     t = threading.Thread(target=task, args=(i,))
+#     t.start()
+
+if __name__ == '__main__':
+    task(1)
+
+
+
+
 
 
 
